7884ed0bfedda4e679c4943b138523014cc211b7
Yosibalacho <yosibala@gmail.com>
2022-05-30 10:18:45 +0300

    add

--- main.js
+++ main.js
@@ -2,3 +2,11 @@
 // second.innerText = "my dom app";
 // var textClass = document.getElementsByClassName("texts");
 // console.log(textClass);
+// console.log(textClass[1]);
+// console.log(textClass[2].innerHTML);
+// var textsClass2 = document.getElementsByClassName("texts2");
+//  console.log(textsClass2);
+// function nameFromUser() {
+//   name1.innerText = prompt("Enter your name");
+// }
+// nameFromUser();
