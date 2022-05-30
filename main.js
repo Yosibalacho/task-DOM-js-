fdf7d0ad476c4ac9455fbb9ad3fbda006b09de44
Yosibalacho <yosibala@gmail.com>
2022-05-30 13:42:15 +0300

    add

--- main.js
+++ main.js
@@ -18,3 +18,41 @@
 //   }
 // }
 // numAndColor();
+//9// לא הושלם
+// function inputTy() {
+//   var inputType = prompt("enter type");
+//   document.innerHTML = <input></input>;
+// }
+// inputTy();
+//11//
+// function changeInnerText(className,text) {
+//     var elemArray=document.getElementsByClassName(className)
+//     for(var i=0;i<elemArray.length;i++){
+//             elemArray[i].innerText=text;
+//     }
+// }
+// changeInnerText("classy","how are you")
+// function pElemets(){
+// var pArray= document.
+// for(var i=0;i<pArray.length;i++){
+// pArray[i].innerHTML=""
+
+// }
+// }
+// pElemets()
+//19// לא הושלם
+function contactInfo() {
+  var numOfContacts = +prompt();
+  for (var i = 0; i < numOfContacts; i++) {
+    var contactObject = {
+      fullName: prompt("Enter full name"),
+      company: prompt("Enter the company name"),
+      phone: prompt("Enter phone"),
+      mobile: prompt("Enter mobile"),
+      email: prompt("Enter email"),
+    };
+    document.getElementById().innerHTML +=
+      "<li>" + contactObject.fullName + "</li>";
+  }
+}
+contactInfo();
